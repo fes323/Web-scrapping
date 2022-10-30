1408c69597d140c08d5a0cfead219eae028c5e1e
Maksim legeyda <Maksim.legeyda@yandex.ru>
2022-10-30 21:36:32 +0300

    50% done Почти готово. Осталось решить проблему с поиском слов внутри постов, а также привести всё это в нормальный вид

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
-import re
-import pandas as pd
 
 
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+KEYWORDS = ['Дизайн', 'Фото', 'Web', 'Python']
 
 URL_TEMPLATE = 'https://habr.com/ru/all/'
 r = requests.get(URL_TEMPLATE)
@@ -17,35 +15,78 @@ post_titles = []
 post_titles.append(soup.find_all('h2', attrs={'data-test-id': 'articleTitle'}))
 
 main_link = 'https://habr.com'
-links = []
+data_links = []
 for item in post_titles:
     for element in item:
-        links.append(element.find('a').get('href'))
+        data_links.append(main_link + element.find('a').get('href'))
+
 
 my_dict = {
     'Дата публикации:': '',
     'Название статьи:': '',
     'Ссылка:': '',
 }
-for link in links:
-    data_link = main_link + link
-    r = requests.get(data_link)
+
+parse_dict = {
+    'Дата публикации:': '',
+    'Название статьи:': '',
+    'Ссылка:': '',
+}
+
+result_pars = []
+data_parse = []
+links_KEYWORD = []
+
+
+for link in data_links:
+
+    data_link = link
+    r = requests.get(link)
     soup = bs(r.text, 'html.parser')
 
-    data_post = soup.find_all('article', class_='tm-article-presenter__content tm-article-presenter__content_narrow')
-    for word in KEYWORDS:
-        N = +1
-        data = soup.find_all(KEYWORDS[N-1])
+    date_of_post = soup.find('time')['title']
+    title_of_post = soup.find('h1', class_='tm-article-snippet__title tm-article-snippet__title_h1')
+    my_dict = ({
+        'Дата публикации:': date_of_post,
+        'Название статьи:': title_of_post.get_text(),
+        'Ссылка:': data_link,
+    })
+    data_parse.append(my_dict)
 
-    if data != None:
-        date_of_post = soup.find('time')['title']
-        title_of_post = soup.find('h1', class_='tm-article-snippet__title tm-article-snippet__title_h1')
-        my_dict.update({
-            'Дата публикации:': date_of_post,
-            'Название статьи:': title_of_post.get_text(),
-            'Ссылка:': data_link,
-        })
-        pprint(my_dict)
-    else:
-        pass
+    post_display = soup.find('article', class_='tm-article-presenter__content tm-article-presenter__content_narrow')
+    #match_chek = post_display.find_all(text=KEYWORDS[0])
+    tag_of_post = post_display.find('div', attrs={'data-test-id': 'articleHubsList'}).get_text()
+
+    for word in KEYWORDS:
+
+        if word in tag_of_post:
+            links_KEYWORD.append(data_link)
+            parse_dict = ({
+                'Дата публикации:': date_of_post,
+                'Название статьи:': title_of_post.get_text(),
+                'Ссылка:': data_link,
+            })
+            result_pars.append(parse_dict)
+
+        text_in_post = []
+        for element in post_display:
+            text_in_post.append(element.find('p'))
+            if word in text_in_post:
+                links_KEYWORD.append(data_link)
+                parse_dict = ({
+                    'Дата публикации:': date_of_post,
+                    'Название статьи:': title_of_post.get_text(),
+                    'Ссылка:': data_link,
+                })
+                result_pars.append(parse_dict)
+
+
+
+
+pprint(result_pars)
+
+#pprint(data_parse)
+
+
+
 
